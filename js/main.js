395cb49bb6089c8485db7a6854f3c1ecbacf4fd5
Коновалов Данил Сергеевич <konovalov@elewise.local>
2022-01-29 17:12:57 +0400

    add Swipe

--- js/main.js
+++ js/main.js
@@ -6,31 +6,69 @@ let buttonRight = SLIDER.querySelector('.right-btn');
 let slidesLength = slides.length;
 
 let currentIndex = 0;
-console.log(slides,buttonLeft,buttonRight,slidesLength)
-buttonRight.onclick = () =>{moveRight()};
+let startTouch = {
+    x: null,
+    y: null
+}
+let endTouch = {
+    x: null,
+    y: null
+}
+console.log(slides, buttonLeft, buttonRight, slidesLength)
+buttonRight.onclick = () => {
+    moveRight()
+};
 buttonLeft.onclick = moveLeft;
+SLIDER.addEventListener('touchstart', (event) => {
+    startTouch.x = event.touches[0].clientX;
+    startTouch.y = event.touches[0].clientY;
+}, false)
+SLIDER.addEventListener('touchmove', checkAction, false)
+
+function checkAction(event) {
+    endTouch.x = event.touches[0].clientX;
+    endTouch.y = event.touches[0].clientY;
+    if (!startTouch.x || !startTouch.y) {
+        return false
+    }
+    let xDiff = endTouch.x - startTouch.x
+    let yDiff = endTouch.y - startTouch.y
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            moveRight()
+        } else {
+            moveLeft()
+        }
+    } else {
+        return false
+    }
+    startTouch.x = null;
+    startTouch.y = null;
+}
+
+changeBackground()
 
 function changeBackground() {
-    document.body.style.backgroundImage=`url(${slides[currentIndex].firstChild.currentSrc})`;
+    document.body.style.backgroundImage = `url(${slides[currentIndex].firstChild.currentSrc})`;
 }
+
 function moveRight() {
     console.log('move')
-    if(currentIndex < slidesLength - 1) {
+    if (currentIndex < slidesLength - 1) {
         currentIndex++
-    }
-    else {
+    } else {
         currentIndex = 0;
     }
     clearActiveClass();
     addActiveClass(currentIndex);
     changeBackground()
 }
+
 function moveLeft() {
-    if(currentIndex !== 0) {
+    if (currentIndex !== 0) {
         currentIndex--
-    }
-    else {
-        currentIndex = slidesLength-1;
+    } else {
+        currentIndex = slidesLength - 1;
     }
     clearActiveClass();
     addActiveClass(currentIndex);
@@ -43,9 +81,10 @@ function clearActiveClass() {
         element.classList.remove('active')
     })
 }
+
 function addActiveClass(current) {
-    slides.forEach((element,index) => {
-        if(index === current) {
+    slides.forEach((element, index) => {
+        if (index === current) {
             element.classList.add('active')
         }
     })
